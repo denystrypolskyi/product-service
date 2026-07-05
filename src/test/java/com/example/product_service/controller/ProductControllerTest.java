6472ef254aef8c2852afec6a7c419de56dcc6805
denystrypolskyi <denystrypolskyi@gmail.com>
2026-07-05 16:13:10 +0200

    Replace deprecated MockBean in product tests

--- src/test/java/com/example/product_service/controller/ProductControllerTest.java
+++ src/test/java/com/example/product_service/controller/ProductControllerTest.java
@@ -12,8 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.product_service.dto.ProductDTO;
@@ -28,10 +28,10 @@ class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private ProductService productService;
 
-    @MockBean
+    @MockitoBean
     private JwtUtil jwtUtil;
 
     @Test
